ebe3153c4239711f032a8de8ce0e043dec885214
Aseneca <princeosuh@gmail.com>
2022-11-03 21:05:42 +0100

    APP Frontend Frame initial design, create table with DOM, implement Navigation Bar bug: none

---
+++ leaderboard.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+  createTable();
+};
+
+function createTable() {
+  //this function creates a table
+  const elements = Elements.GetElements();
+  const heading = Elements.GetHeading();
+  const properties = Elements.GetProperties();
+
+  //create a table
+  const table = document.createElement("table");
+
+  //Include a caption
+  table.createCaption().textContent =
+    " Loggers Ranking. Thank you for using Toolhub, and come 🔙 anytime!";
+
+  //insert a row; add headings
+  const horizontalRow = table.insertRow();
+  for (let title of heading) {
+    horizontalRow.insertCell(-1).outerHTML = `<th>${title}</th>`;
+  }
+
+  //iterating through data; adding rows and cells for each element
+  for (let element of elements) {
+    //defining a variable for the table cells
+    const eachCell = table.insertRow(-1);
+    for (let property of properties) {
+      eachCell.insertCell(-1).innerHTML = element[property];
+    }
+  }
+
+  document.getElementById("tableinsert").appendChild(table);
+}
---
+++ leaderboardlist.js
@@ -0,0 +1,96 @@
+class Elements {
+  static GetHeading() {
+    return ["Name", "Score"];
+  }
+  static GetProperties() {
+    return ["userName", "score"];
+  }
+  static GetElements() {
+    return [
+      {
+        userName: "John",
+        score: 50,
+      },
+      {
+        userName: "Jane",
+        score: 30,
+      },
+      {
+        userName: "Anna",
+        score: 25,
+      },
+      {
+        userName: "Joana",
+        score: 70,
+      },
+      {
+        userName: "Justin",
+        score: 10,
+      },
+      {
+        userName: "Peter",
+        score: 20,
+      },
+      {
+        userName: "Constance",
+        score: 60,
+      },
+      {
+        userName: "Trump",
+        score: 85,
+      },
+      {
+        userName: "Herrietta",
+        score: 5,
+      },
+      {
+        userName: "Mpabbe",
+        score: 90,
+      },
+      {
+        userName: "Televiv",
+        score: 13,
+      },
+      {
+        userName: "henry",
+        score: 30,
+      },
+      {
+        userName: "David",
+        score: 22,
+      },
+      {
+        userName: "Jonas",
+        score: 61,
+      },
+      {
+        userName: "Chris",
+        score: 62,
+      },
+      {
+        userName: "Jude",
+        score: 18,
+      },
+      {
+        userName: "Mark",
+        score: 46,
+      },
+      {
+        userName: "Tony",
+        score: 66,
+      },
+      {
+        userName: "Koffi",
+        score: 55,
+      },
+      {
+        userName: "Toffee",
+        score: 100,
+      },
+      {
+        userName: "Gina",
+        score: 45,
+      },
+    ];
+  }
+}
