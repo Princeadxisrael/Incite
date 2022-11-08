d3631f066c38bb5120b9f1b354a2bc22cfa62185
Aseneca <princeosuh@gmail.com>
2022-11-08 16:44:18 +0100

    minor change on font size

--- records.js
+++ records.js
@@ -4,3 +4,19 @@
 const menuIconEL = document.querySelector(".menu-icon");
 const sideBarEL = document.querySelector(".sidebar");
 const sideBarCLoseEl = document.querySelector(".sidebar_close-icon");
+
+const switchClassName = function (el, className) {
+  if (el.classList.contains(className)) {
+    el.classList.remove(className);
+  } else {
+    el.classList.add(className);
+  }
+};
+
+menuIconEL.addEventListener("click", function () {
+  switchClassName(sideBarEL, "sidebar-active");
+});
+
+sideBarCLoseEl.addEventListener("click", function () {
+  switchClassName(sideBarEL, "sidebar-active");
+});
